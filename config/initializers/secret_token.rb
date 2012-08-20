b2c70531ed5f30f9cfeeefdb7849cfda032c9274
Rodrigo Estebanez <restebanez@mdsol.com>
2012-08-21 00:03:35 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodAllergyTracker::Application.config.secret_token = 'bdab8e82b215153f7ffb9c1cdf16eb4c501dfd0a2ee4a02234841a2dea9f500b06b4905d5e5e61b53df3097fbf882a5b9d2fbbc36735c7a8b2d456459944e5c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FoodAllergyTracker::Application.config.session_store :cookie_store, key: '_food_allergy_tracker_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FoodAllergyTracker::Application.config.session_store :active_record_store
